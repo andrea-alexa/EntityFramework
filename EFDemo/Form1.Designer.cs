7a428bbe8cfd6e495b3059c82d4378b9aa4ffd34
AndreaN <nm0753032023@unab.edu.sv>
2024-09-25 01:39:37 -0600

    Guia 1

--- EFDemo/Form1.Designer.cs
+++ EFDemo/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnObtenerTodos = new System.Windows.Forms.Button();
+            this.btnTodos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtObtenerTodos = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(12, 12);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.RowHeadersWidth = 51;
+            this.dgvCustomers.RowTemplate.Height = 24;
+            this.dgvCustomers.Size = new System.Drawing.Size(1235, 390);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // btnObtenerTodos
+            // 
+            this.btnObtenerTodos.Location = new System.Drawing.Point(548, 463);
+            this.btnObtenerTodos.Name = "btnObtenerTodos";
+            this.btnObtenerTodos.Size = new System.Drawing.Size(153, 59);
+            this.btnObtenerTodos.TabIndex = 1;
+            this.btnObtenerTodos.Text = "Cargar Todos";
+            this.btnObtenerTodos.UseVisualStyleBackColor = true;
+            this.btnObtenerTodos.Click += new System.EventHandler(this.btnObtenerTodos_Click);
+            // 
+            // btnTodos
+            // 
+            this.btnTodos.Location = new System.Drawing.Point(204, 481);
+            this.btnTodos.Name = "btnTodos";
+            this.btnTodos.Size = new System.Drawing.Size(75, 23);
+            this.btnTodos.TabIndex = 2;
+            this.btnTodos.Text = "obtener";
+            this.btnTodos.UseVisualStyleBackColor = true;
+            this.btnTodos.Click += new System.EventHandler(this.btnTodos_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 484);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Customer ID";
+            // 
+            // txtObtenerTodos
+            // 
+            this.txtObtenerTodos.Location = new System.Drawing.Point(98, 481);
+            this.txtObtenerTodos.Name = "txtObtenerTodos";
+            this.txtObtenerTodos.Size = new System.Drawing.Size(100, 22);
+            this.txtObtenerTodos.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1259, 662);
+            this.Controls.Add(this.txtObtenerTodos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTodos);
+            this.Controls.Add(this.btnObtenerTodos);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnObtenerTodos;
+        private System.Windows.Forms.Button btnTodos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtObtenerTodos;
     }
 }
 
